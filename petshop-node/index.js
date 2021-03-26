bbd326f6a297fd89cb16461b38df0f7dc2a2971a
mateusntn <mateusantoniodev@gmail.com>
2021-03-26 15:16:48 -0300

    add the database

--- petshop-node/index.js
+++ petshop-node/index.js
@@ -1,33 +1,5 @@
-const dogs = [
-    {
-        name: 'boltz',
-        age: 3,
-        tutor: 'junior',
-        isVaccinated: true,
-        services: [
-            { typeServ: 'banho', date: '02/15/2021'},
-            { typeServ: 'tosa', date: '02/15/2021'}
-        ]
-    },
-    {
-        name: 'Kiara',
-        age: 5,
-        tutor: 'Igor',
-        isVaccinated: true,
-        services: [
-            { typeServ: 'tosa', date: '01/27/2021'}
-        ]
-    },
-    {
-        name: 'Coragem',
-        age: 8,
-        tutor: 'Eustácio',
-        isVaccinated: false,
-        services: [
-            { typeServ: 'banho', date: '03/07/2021'},
-        ]
-    }
-];
+const database = require('./database.json'); 
+let dogs = database.dogs;
 
 const listDogs = () => {
     for(let dog of dogs) {
