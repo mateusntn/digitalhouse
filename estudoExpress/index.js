a49ae98bccd85849715e1f7263e716ded95a309b
mateusntn <mateusantoniodev@gmail.com>
2021-03-31 14:14:22 -0300

    Creates the .get .post .put .delete

--- estudoExpress/index.js
+++ estudoExpress/index.js
@@ -1,4 +1,46 @@
 const express = require('express');
 const app = express();
+const { v4:uuidv4 } = require('uuid');
 
-console.log(app);
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (req, res) => {
+    const {title, owner} = req.query;
+
+    console.log(title);
+    console.log(owner);
+
+    return res.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
+});
+
+app.post('/projects', (req, res) => {
+    const {title, owner} = req.body;
+    const project = {id: uuidv4(), title, owner};
+    projects.push(project);
+    return res.json(project);
+});
+
+app.put('/projects/:id', (req, res) => {
+    const params = req.params;
+    console.log(params);
+    return res.json([
+        'Projeto 50',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+    ])
+});
+
+app.delete('/projects/:id', (req, res) => {
+    return res.json([
+        'Projeto 50',
+        'Projeto 2',
+    ])
+});
+
+app.listen(3000);
