9b8d15258d80e38e2dcd060afd184ebc186e7040
mateusntn <mateusantoniodev@gmail.com>
2021-03-31 14:45:30 -0300

    Changes the app.get

--- estudoExpress/index.js
+++ estudoExpress/index.js
@@ -7,15 +7,7 @@ app.use(express.json());
 const projects = [];
 
 app.get('/projects', (req, res) => {
-    const {title, owner} = req.query;
-
-    console.log(title);
-    console.log(owner);
-
-    return res.json([
-        'Projeto 1',
-        'Projeto 2',
-    ]);
+    return res.json(projects);
 });
 
 app.post('/projects', (req, res) => {
