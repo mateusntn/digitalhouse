7f58281996000f06a7476c0246059a401684e5b8
mateusntn <mateusantoniodev@gmail.com>
2021-04-01 17:10:33 -0300

    Creates app.get and app.post

--- petshop-express/index.js
+++ petshop-express/index.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const app = express();
+
+const petshop = require('./petshop');
+
+app.use(express.json());
+
+app.get('/pets', (req, res) => {
+    return res.send(petshop.listDogs())
+});
+
+app.post('/pets', (req,res)=> {
+    const { name, age, tutor, isVaccinated, services} = req.body
+    const newPet = { name, age, tutor, isVaccinated, services };
+    petshop.addDog(newPet);
+    return res.json(newPet);
+})
+
+
+app.listen(3000);
