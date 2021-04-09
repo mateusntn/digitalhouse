c836b5faa0c339a1ca9790bb0ae21b1c979a7eb9
mateusntn <mateusantoniodev@gmail.com>
2021-04-09 15:19:22 -0300

    Creates the Comentario model

---
+++ estudoSequelize/models/Comentario.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Comentario = sequelize.define(
+        'Comentario',{
+            texto: DataTypes.STRING,
+            usuarios_id: DataTypes.INTEGER,
+            posts_id: DataTypes.INTEGER
+        }, {
+            tableName: "comentarios",
+            timestamps: false
+        }
+    );
+
+    return Comentario;
+}
