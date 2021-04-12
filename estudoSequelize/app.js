3fec9990c3f69ae2dcfd6ebaed15086914dc3b46
mateusntn <mateusantoniodev@gmail.com>
2021-04-12 12:07:23 -0300

    Creates the list function

--- estudoSequelize/app.js
+++ estudoSequelize/app.js
@@ -1,58 +1,8 @@
 const { Usuario, Post, Comentario, sequelize } = require('./models');
+const { Op } = require('sequelize');
 
-Usuario.findAll()
-.then((result) => {
-    console.table(result.map(user => user.toJSON()))
-});
-
-Usuario.findByPk(2)
-.then((result) => {
-    console.table(result.toJSON());
-});
-
-Usuario.findOne({
-    where : {
-        nome : 'Hendy Almeida'
-    }
-})
-.then((result) => {
-    console.table(result.toJSON());
-});
-
-Post.findAll()
-.then((result) => {
-    console.table(result.map(user => user.toJSON()))
-});
-
-Post.findByPk(2)
-.then((result) => {
-    console.table(result.toJSON());
-});
-
-Post.findOne({
-    where : {
-        texto : 'Como foi o fim de semana?'
-    }
-})
-.then((result) => {
-    console.table(result.toJSON());
-});
-
-Comentario.findAll()
-.then((result) => {
-    console.table(result.map(user => user.toJSON()))
-});
-
-Comentario.findByPk(1)
-.then((result) => {
-    console.table(result.toJSON());
-});
-
-Comentario.findOne({
-    where : {
-        texto : 'Oi gente é o !@#!$%! Meu nome agora é Zé Pequeno!'
-    }
-})
-.then((result) => {
-    console.table(result.toJSON());
-});
+const list = (table, conditions) => {
+    table.findAll(conditions).then((result) => {
+        console.table(result.map(unity => unity.toJSON()))
+    });
+}
