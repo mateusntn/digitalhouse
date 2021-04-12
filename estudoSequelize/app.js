1e4d422c61e5b960754d72b4299ac31c18318120
mateusntn <mateusantoniodev@gmail.com>
2021-04-12 12:07:56 -0300

    Creates the listByPk function

--- estudoSequelize/app.js
+++ estudoSequelize/app.js
@@ -6,3 +6,10 @@ const list = (table, conditions) => {
         console.table(result.map(unity => unity.toJSON()))
     });
 }
+
+const listByPk = (table, id) => {
+    table.findByPk(id)
+    .then((result) => {
+        console.table(result.toJSON());
+    });
+}
