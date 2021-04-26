c38346434aed315a1f4acef6f3befc4e985ca103
mateusntn <mateusantoniodev@gmail.com>
2021-04-26 13:24:55 -0300

    Changes dark mode

--- lovt-desafio/scripts/main.js
+++ lovt-desafio/scripts/main.js
@@ -6,14 +6,5 @@ elementHeader.insertAdjacentHTML('afterend',`<strong>Olá, ${username}</strong>`
 
 
 const darkMode = () => {
-    let element = document.body;
-    element.classList.toggle("body-darkmode");
-
-    let footer = document.querySelector('footer');
-    footer.classList.toggle("darkmode")
-
-    let descricao = document.querySelectorAll('div.descricao')
-    console.log(descricao)
-    descricao[0].classList.toggle("darkmode")
-    descricao[1].classList.toggle("darkmode")
+    document.body.classList.toggle("darkmode");
 }
